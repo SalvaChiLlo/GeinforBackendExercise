dbd0b00dc031f9ea34c681f0319ef57f49b95b0c
SalvaChLl <salvachll@gmail.com>
2021-11-12 18:59:50 +0100

    First commit

---
+++ src/main/java/com/salvachll/geinfor_exercise/GeinforExerciseApplication.java
@@ -0,0 +1,12 @@
+package com.salvachll.geinfor_exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeinforExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeinforExerciseApplication.class, args);
+	}
+}
---
+++ src/test/java/com/salvachll/geinfor_exercise/GeinforExerciseApplicationTests.java
@@ -0,0 +1,15 @@
+package com.salvachll.geinfor_exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class GeinforExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertTrue(true);
+	}
+
+}
